e877c8b14aabe63bc41831194fbf09ddda904576
Gustavo Tonetto <100966526+gustavodt@users.noreply.github.com>
2023-12-06 16:23:39 -0300

    Update urlapi.dart, mudado os nomes das classe para padronização

--- lib/modelos/urlapi.dart
+++ lib/modelos/urlapi.dart
@@ -1,8 +1,8 @@
 class  Urlapi{
   late String urlAutenticacaoPedidos = "http://localhost:3000/auth";
   late String urlPedidos = "http://localhost:3000/pedidos?status=PRONTO_PARA_COLETA&pagina=0";
-  late String urlLoginLogistica = "http://localhost:9089/auth";
+  late String urlAutenticacaoLogistica = "http://localhost:9089/auth";
   late String urlMarkeplace = "https://cardapios-mktplace-api-production.up.railway.app/";
   late String urlLogistica = "http://localhost:9089/";
-  late String urlLoginCadastros = "https://gestao-de-cadastros-api-production.up.railway.app/auth";
+  late String urlAutenticacaoCadastros = "https://gestao-de-cadastros-api-production.up.railway.app/auth";
 }
